462214dbccc6377fcf6ba039129a4c08653b2da9
Howard Feger <hfeger4@gmail.com>
2018-05-07 23:12:09 -0700

    add mergeSort

--- practice.js
+++ practice.js
@@ -12,3 +12,39 @@ function removeDups(nums){
 }
 
 console.log(removeDups([1,1,1,2,2,3,3,3,3,3,3]));
+
+//mergesort
+
+function mergeSort(arr){
+  if (arr.length === 1){
+    return arr;
+  }
+
+  let mid = Math.floor(arr.length/2);
+  let left = arr.slice(0,mid);
+  let right = arr.slice(mid);
+
+  let sortedLeft = mergeSort(left);
+  let sortedRight = mergeSort(right);
+  return merge(sortedLeft, sortedRight);
+}
+
+function merge(left,right){
+  let arr = [];
+  while (left.length > 0 && right.length > 0){
+    if (left[0] < right[0]){
+      arr.push(left.shift());
+    }else{
+      arr.push(right.shift());
+    }
+  }
+  if (left.length > 0){
+    arr.concat(left);
+  }
+  if (right.length > 0){
+    arr.concat(right);
+  }
+  return arr.concat(left).concat(right);
+}
+
+console.log(mergeSort([1,5,2,3,4]));
